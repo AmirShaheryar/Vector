fe46ce521b2671106e59fed9adbd755c5400e262
Shaheryar Amir <shaheryaraamir13@gmail.com>
2026-08-05 19:50:49 +0500

    Push commit

--- vector.cpp
+++ vector.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+using namespace std;
 class Vector {
 private:
         double* data;
@@ -14,5 +16,26 @@ public:
         }            
         int getSize() const {
             return size;
+        }
+
+        void Push(double value) {
+            if (size == 0) 
+            {
+                data = new double[1];
+                data[0] = value;
+                size = 1;
+            } 
+            else 
+            {
+                double* newData = new double[size + 1];
+                for (int i = 0; i < size; ++i) 
+                {
+                    newData[i] = data[i];
+                }
+                newData[size] = value;
+                delete[] data;
+                data = newData;
+                ++size;
+            }
         }
 };
